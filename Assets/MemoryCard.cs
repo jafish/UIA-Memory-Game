10c30b61745794e401ca430335eb67f7e5cec968
Josh Fishburn <josh@joshfishburn.com>
2018-10-11 22:43:13 -0400

    Initial Commit
    Worked through chapter up to page 115.

---
+++ Assets/MemoryCard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour {
+    [SerializeField] private GameObject cardBack;
+    [SerializeField] private SceneController controller;
+
+    private int _id;
+
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf) {
+            cardBack.SetActive(false);
+        }
+    }
+
+    public int id {
+        get { return _id; }
+    }
+
+    public void SetCard (int id, Sprite image) {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+}
---
+++ Assets/SceneController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour {
+    [SerializeField] private MemoryCard originalCard;
+    [SerializeField] private Sprite[] images;
+
+	// Use this for initialization
+	void Start () {
+        int id = Random.Range(0, images.Length);
+        originalCard.SetCard(id, images[id]);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
